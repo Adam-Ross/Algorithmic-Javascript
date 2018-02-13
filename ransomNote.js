a622db53c6b685ced9223cf5a5ef83f34ab57ebf
Adam Ross <adamgarrettross@gmail.com>
2018-02-13 17:54:56 -0600

    JS algos

--- ransomNote.js
+++ ransomNote.js
@@ -27,29 +27,3 @@ var note = 'bring the money downtown'
 var magazineText = 'the downtown is a cool place to visit but make sure that you bring money'
 
 ransomNote(note, magazineText);
-
-// function caesarCipher (str, num) {
-//   num = num % 26;
-//   var lowerCaseStr = str.toLowerCase();
-//   var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
-//   var newStr = ''
-//
-//   for(var i = 0; i < lowerCaseStr.length; i++) {
-//     var currentLetter = lowerCaseStr[i];
-//     if(currentLetter === ' ') {
-//       newStr += currentLetter;
-//       continue;
-//     }
-//     var currentIndex = alphabet.indexOf(currentLetter);
-//     var newIndex = currentIndex + num;
-//     if(newIndex > 25) newIndex = newIndex - 26;
-//     if(newIndex < 0) newIndex =  26 + newIndex;
-//     if(str[i] === str[i].toUpperCase()) {
-//       newStr += alphabet[newIndex].toUpperCase();
-//     }
-//     else newStr += alphabet[newIndex];
-//   }
-//   return newStr
-// }
-
-// console.log(caesarCipher('Ybyk Ambcp gq y hytyqapgnr ugxypb', 2)) // Bqq Mggrgt
