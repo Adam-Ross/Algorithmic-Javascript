18e1d2dd6d9ae49845adb15a238335b94bb295d7
Adam Ross <adamgarrettross@gmail.com>
2018-02-15 11:28:07 -0600

    max character algo

--- maxCharacterInString.js
+++ maxCharacterInString.js
@@ -1,28 +1,31 @@
 // Given a string, return the character that has the hightest number of repeats.
+// Watch my tutorial 'bout it. https://www.youtube.com/watch?v=q32hP1x0jSY&feature=youtu.be
 
 function maxCharInString(str) {
-  const charMap = {};
+  const strObj = {};
   let max = 0;
   let maxChar = '';
 
-  for(let char of str) {
-    if (charMap[char]) {
-      charMap[char]++;
+
+  for(let char of str){
+    if(strObj[char]) {
+      strObj[char]++;
     } else {
-      charMap[char] = 1;
+      strObj[char] = 1;
     }
   }
-  for (let char in charMap) {
-    if(charMap[char] > max) {
-      max = charMap[char];
+
+  for(let char in strObj) {
+    if(strObj[char] > max) {
+      max = strObj[char];
       maxChar = char;
     }
   }
   console.log(maxChar);
 }
 
-
 var testCase = 'aaabbbcccfffffdddd' // should return 'f'
 var testCase2 = 'aaabbbcccdddd' // should return 'd'
+
 maxCharInString(testCase);
 maxCharInString(testCase2);
