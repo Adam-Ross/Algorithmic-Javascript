ab893bc07e360d2d2371c3f5f5e6c356c512e820
Adam Ross <adamgarrettross@gmail.com>
2018-02-17 16:07:19 -0600

    More on map.

--- notes/1-arrayObjectHelpers/map.js
+++ notes/1-arrayObjectHelpers/map.js
@@ -1,18 +1,30 @@
-// Really widely used, man - so, better know it.
-// Returns in new array, member that.
+// // Really widely used, man - so, better know it.
+// // Returns in new array, member that.
+//
+// let numbers = [1,2,3];
+// let doubled = [];
+//
+// // using a for loop.
+//
+// for(var i = 0; i < numbers.length; i++) {
+//   doubled.push(numbers[i] * 2);
+// }
+// console.log(doubled);
+//
+// // now, using map.
+//
+// numbers.map(number => {
+//   return number * 2;
+// });
 
-let numbers = [1,2,3];
-let doubled = [];
+// putting it in a function, remember to use the return for both the funciton and
+// the map method itself.
 
-// using a for loop.
+const numbers = [1,2,3,4];
 
-for(var i = 0; i < numbers.length; i++) {
-  doubled.push(numbers[i] * 2);
+function double(arr) {
+	return arr.map(number => {
+		return number * 2;
+  });
 }
-console.log(doubled);
-
-// now, using map.
-
-numbers.map(number => {
-  return number * 2;
-});
+console.log(double(numbers));
