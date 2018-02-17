04b24971cccdf4b0942e3ae8251909ba873c76bc
Adam Ross <adamgarrettross@gmail.com>
2018-02-17 16:36:37 -0600

    enhanced object literals.

---
+++ notes/2-es6Sugar/enhancedObjecLiterals.js
@@ -0,0 +1,23 @@
+// Create an object literal for a bookstor object that can calculate the invertory
+// value and can also find books values based off title.
+
+function createBookstore(inventory) {
+  return {
+    inventory,
+    inventoryValue() {
+      return this.inventory.reduce((total, book) => total + book.price, 0);
+    },
+    priceForTitle(title) {
+      return this.inventory.find(book => book.title === title).price;
+    }
+  };
+}
+
+const inventory = [
+  { title: 'Harry potter', price: 10},
+  { title: 'Harry potter part 2', price: 12},
+]
+
+const bookshop = createBookstore(inventory);
+bookshop.inventoryValue();
+bookshop.priceForTitle('Harry potter');
