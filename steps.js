78bb90e5ea8f22bfbe4a20f4d5f92c5b9caf48d3
Adam Ross <adamgarrettross@gmail.com>
2018-02-18 14:38:52 -0600

    steps

--- steps.js
+++ steps.js
@@ -21,19 +21,3 @@ function steps(n) {
 }
 
 steps(25);
-
-function step(n) {
-  for(let row = 0; row < n; row++) {
-    let stair = '';
-
-    for(let col = 0; col < n; col++) {
-      if(col <= row) {
-        stair += '#';
-      } else {
-        stair += ' ';
-      }
-    }
-    console.log(stair);
-  }
-}
- step(25);
