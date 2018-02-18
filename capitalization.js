64b618013ef39f5e6d380cc05600293a3c04c425
Adam Ross <adamgarrettross@gmail.com>
2018-02-18 13:51:53 -0600

    More with capitalization

---
+++ capitalization.js
@@ -0,0 +1,29 @@
+// Capitalize every word in a given string.
+
+function cap(str) {
+  let arr = str.split(' ');
+  let newStr = [];
+  for(let word of arr) {
+    newStr.push(word[0].toUpperCase() + word.slice(1))
+  }
+  return newStr.join(' ');
+}
+
+console.log(cap('this is the string. and thank you about it.'));
+
+// Alternate way of solving.
+
+function cap(str) {
+  let result = str[0].toUpperCase();
+
+  for(let i = 1; i < str.length; i++) {
+    if(str[i - 1] === ' ') {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+}
+
+console.log(cap('This is the string.'));
