ffe7b3a7395474195f99317cf42402141afe99cd
Adam Ross <adamgarrettross@gmail.com>
2018-02-22 14:15:56 -0600

    chunking

--- arrayChunking.js
+++ arrayChunking.js
@@ -17,35 +17,49 @@ let number = 2;
 //   }
 //   return chunked;
 // }
-//
+
 // console.log(chunk(arr, number));
 
 
-function chunk(arr, size) {
-  const chunked = [];
+// function chunk(arr, size) {
+//   const chunked = [];
+//
+//   for (let element of arr) {
+//     let last = chunked[chunked.length -1];
+//     if(!last || last.length === size) {
+//       chunked.push([element]);
+//     } else {
+//       last.push(element);
+//     }
+//   }
+//   return chunked;
+// }
 
-  for (let element of arr) {
-    let last = chunked[chunked.length -1];
+function chunked(arr, size) {
+  const chunk = [];
+
+  for(let elem of arr) {
+    let last = chunk[chunk.length - 1];
     if(!last || last.length === size) {
-      chunked.push([element]);
+      chunk.push([elem]);
     } else {
-      last.push(element);
+      last.push(elem);
     }
   }
-  return chunked;
+  return chunk;
 }
 
-// console.log(chunk(arr, number));
+console.log(chunked(arr, number));
 
-function chunk2(array, size) {
-  const chunked = [];
-  let index = 0;
-
-  while (index < array.length){
-    chunked.push(array.slice(index, index + size));
-    index += size;
-  }
-  return chunked;
-}
-
-console.log(chunk2(arr, number));
+// function chunk2(array, size) {
+//   const chunked = [];
+//   let index = 0;
+//
+//   while (index < array.length){
+//     chunked.push(array.slice(index, index + size));
+//     index += size;
+//   }
+//   return chunked;
+// }
+//
+// console.log(chunk2(arr, number));
--- maxCharacterInString.js
+++ maxCharacterInString.js
@@ -1,13 +1,35 @@
 // Given a string, return the character that has the hightest number of repeats.
 // Watch my tutorial 'bout it. https://www.youtube.com/watch?v=q32hP1x0jSY&feature=youtu.be
 
-function maxCharInString(str) {
+// function maxCharInString(str) {
+//   const strObj = {};
+//   let max = 0;
+//   let maxChar = '';
+//
+//
+//   for(let char of str){
+//     if(strObj[char]) {
+//       strObj[char]++;
+//     } else {
+//       strObj[char] = 1;
+//     }
+//   }
+//
+//   for(let char in strObj) {
+//     if(strObj[char] > max) {
+//       max = strObj[char];
+//       maxChar = char;
+//     }
+//   }
+//   console.log(maxChar);
+// }
+
+function maxChar(str) {
   const strObj = {};
   let max = 0;
   let maxChar = '';
 
-
-  for(let char of str){
+  for(let char of str) {
     if(strObj[char]) {
       strObj[char]++;
     } else {
@@ -21,11 +43,11 @@ function maxCharInString(str) {
       maxChar = char;
     }
   }
-  console.log(maxChar);
+  return maxChar;
 }
 
 var testCase = 'aaabbbcccfffffdddd' // should return 'f'
 var testCase2 = 'aaabbbcccdddd' // should return 'd'
 
-maxCharInString(testCase);
-maxCharInString(testCase2);
+console.log(maxChar(testCase));
+console.log(maxChar(testCase2));
