1ebcb81d82e29a22fdfe0896c2e93c2cd8c8d9f5
Adam Ross <adamgarrettross@gmail.com>
2018-03-06 00:26:48 -0600

    ToSum

--- capitalization.js
+++ capitalization.js
@@ -1,31 +1,15 @@
 // Capitalize every word in a given string.
 
-let word = 'word word word'
-
-function capitalize(str) {
-  let newStr = [];
+// better way to do it.
+function capAll(str) {
   str = str.split(' ');
+  let result = [];
 
   for(let word of str) {
-    newStr.push(word[0].toUpperCase() + word.slice(1));
+    result.push(word[0].toUpperCase() + word.slice(1));
   }
-  return newStr.join(' ');
+  return result.join(' ');
 }
-
-console.log(capitalize(word));
-
-function cap(str) {
-  let arr = str.split(' ');
-  let newStr = [];
-  for(let word of arr) {
-    newStr.push(word[0].toUpperCase() + word.slice(1))
-  }
-  return newStr.join(' ');
-}
-
-console.log(cap('this is the string. and thank you about it.'));
-
-// Alternate way of solving.
 
 function cap(str) {
   let result = str[0].toUpperCase();
@@ -40,4 +24,72 @@ function cap(str) {
   return result;
 }
 
-console.log(cap('This is the string.'));
+let sentence = 'This is a string and every word should be capitalized. Thanks, man.'
+console.log(capAll(sentence));
+console.log(cap(sentence));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function capAll(str) {
+//   let result = [];
+//
+//   str = str.split(' ');
+//
+//   for(let word of str) {
+//     result.push(word[0].toUpperCase() + word.slice(1));
+//   }
+//   return result.join(' ');
+// }
+// let str = 'This is a string, and every word should be capitalized.'
+// console.log(capAll(str));
+
+
+
+
+
+
+// Alternate way of solving.
+// function cap(str) {
+//   let result = str[0].toUpperCase();
+//
+//   for(let i = 1; i < str.length; i++) {
+//     if(str[i - 1] === ' ') {
+//       result += str[i].toUpperCase();
+//     } else {
+//       result += str[i];
+//     }
+//   }
+//   return result;
+// }
+// console.log(cap('This is the string.'));
--- toSum.js
+++ toSum.js
@@ -1,3 +1,100 @@
+// write a function that returns all possible combinations of elements within
+// a givin array that add up to given n.
+
+// given array: [1,2,3,4,5,6,7,8,9]
+// given number: 10
+// should return: [ [ 6, 4 ], [ 7, 3 ], [ 8, 2 ], [ 9, 1 ] ]
+
+function sums(arr, sum) {
+  let pairs = [];
+  let numList = [];
+
+  for(let i = 0; i < arr.length; i++) {
+    let currNum = arr[i];
+    let diff = sum - currNum;
+    if(numList.includes(diff)) {
+      pairs.push([currNum, diff]);
+    }
+    numList.push(currNum);
+  }
+  return pairs;
+}
+
+let num = 10;
+let arr = [1,2,3,4,5,6,7,8,9];
+
+console.log(sums(arr, num));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function twoSum(numArr, sum) {
+//   let pairs = [];
+//   let numList = [];
+//
+//   for(let i = 0; i < numArr.length; i++) {
+//     let currNum = numArr[i];
+//     let diff = sum - currNum;
+//
+//     if(numList.includes(diff)) {
+//       pairs.push([currNum, diff]);
+//     }
+//     numList.push(currNum);
+//   }
+//   return pairs;
+// }
+//
+// let arr = [1,2,3,4,5,6,7,8,9];
+// let num = 10;
+//
+// console.log(twoSum(arr, num));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // function twoSum(numArr, sum) {
 //   let pairs = [];
 //   let numList = [];
@@ -12,31 +109,3 @@
 //   }
 //   return numList;
 // }
-
-function twoSum(numArr, sum) {
-  let pairs = [];
-  let hashTable = [];
-
-  for(let i = 0; i < numArr.length; i++) {
-    let currNum = numArr[i];
-    let counterPart = sum - currNum;
-    if(hashTable.indexOf(counterPart) !== -1) {
-      pairs.push([currNum, counterPart]);
-    }
-    hashTable.push(currNum);
-  }
-  return pairs;
-}
-
-const arr = [1,2,3,4,5,6,7,8,9];
-const num = 10;
-
-console.log(twoSum(arr, num));
-
-// let arr = [[1,2], [2,1], [3,4]];
-// arr.forEach(function (elem) {
-//   return elem.sort()
-// });
-//
-// console.log(arr);
-// console.log(arr.uniq)
